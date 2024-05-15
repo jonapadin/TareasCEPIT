fd78292aa92e35f3db970995762b484fdce63b81
Jonathan Padin <padinjonathan1@gmail.com>
2024-05-14 21:40:56 -0300

    carpeta de la clase 6

---
+++ clase_6/elementoMayor.ts
@@ -0,0 +1,20 @@
+import * as rls from 'readline-sync'
+//Creamos el array y la variable donde se va a almacenar el bucle cuando un numero sea mayor
+let arregloNumeros : number[]  = new Array (4,7,9,3,1,45,67,23,29,78,11,16);
+let numeroMayor : number = arregloNumeros[0];
+
+//creamos un bucle
+for(let i = 0; i < 12; i++){
+    if (arregloNumeros[i] > numeroMayor){
+        numeroMayor = arregloNumeros[i]
+    }
+}
+
+function parImpar(numero: number): string {
+    if (numero % 2 === 0) {
+        return "Par";
+    } else {
+        return "Impar";
+    }
+}
+console.log("El numero mas grande del arreglo es:", numeroMayor, " y es", parImpar(numeroMayor));
