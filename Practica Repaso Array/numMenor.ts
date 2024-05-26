186ff64b24d0978232c040e0f4401f2036ef20ac
Jonathan Padin <padinjonathan1@gmail.com>
2024-05-25 22:53:14 -0300

    Tarea de practica

--- Practica Repaso Array/numMenor.ts
+++ Practica Repaso Array/numMenor.ts
@@ -14,18 +14,18 @@ function llenarArray(arreglo: number[], cantidad: number){
     }
 }
 
-//Creamos una funcion para comparar los elementos y buscar el mayor, a mayor le doy el valor del numero que se encuentra en la primera posicion del arreglo, en el for igualo a i en 1 para que empiece a comparar con el segundo valor de la lista, entonces, en el if pregunto si mi segundo num es mayor al valor que se encuentra en mayor, en este caso va a ser el primer valor de la lista y lo guardo en mayor,retornando el num mayor
-function numMayor(arreglo: number[], cantidad: number){
+//Creamos una funcion para comparar los elementos y buscar el menor, a menor le doy el valor del numero que se encuentra en la primera posicion del arreglo, en el for igualo a i en 1 para que empiece a comparar con el segundo valor de la lista, entonces, en el if pregunto si mi segundo num es menor al valor que se encuentra en menor, en este caso va a ser el primer valor de la lista y lo guardo en menor,retornando el num menor
+function numMenor(arreglo: number[], cantidad: number){
 let i: number;
-let mayor : number = arreglo[0];
+let menor : number = arreglo[0];
 for(i = 1; i < cantidad; i++){   
-    if(arreglo[i] > mayor ){
-        mayor = arreglo[i];
+    if(arreglo[i] < menor ){
+        menor = arreglo[i];
     }
     } 
-    return mayor;
+    return menor;
 }
 
-//llenamos el arreglo y mostramos por consola el numero mayor
+//llenamos el arreglo y mostramos por consola el numero menor
 llenarArray(arregloNumeros, longitudArreglo);
-console.log(numMayor(arregloNumeros, longitudArreglo));
+console.log(numMenor(arregloNumeros, longitudArreglo));
