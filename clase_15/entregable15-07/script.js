58c9a61d5de05329abae10723125546863947b29
Jonathan Padin <padinjonathan1@gmail.com>
2024-07-13 19:11:37 -0300

    entregable 15-07

--- clase_15/entregable15-07/script.js
+++ clase_15/entregable15-07/script.js
@@ -1,16 +1,5 @@
 "use strict";
 
-let btn1 = document.getElementById('btn1');
-btn1.addEventListener('click', function(e){
-    mostrar(div)
-})
-
-let btn2 = document.getElementById('btn2');
-btn2.addEventListener('click', function(e){
-    mostrar(div)
-})
-
-
 function mostrar(div) {
     let div1 = document.getElementById('btn1');
     let div2 = document.getElementById('btn2');
